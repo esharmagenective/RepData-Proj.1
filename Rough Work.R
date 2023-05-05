93bb5c320b85e8a3f959eade81c480c098049bd5
esharmagenective <esha.sharma@genective.com>
2023-05-05 11:40:02 -0500

    Start of graphing total number of steps per day

---
+++ Rough Work.R
@@ -0,0 +1,17 @@
+### Rough Work for project
+
+activity<-read.csv("activity.csv")
+
+str(activity)
+steps<-tapply(activity$steps, activity$date, FUN=sum, na.rm=TRUE)
+dates<-unique(activity$date)
+stepssum<-data.frame(Steps=steps, Date=dates)
+
+library(ggplot2)
+
+a<-ggplot(stepssum,aes(x= Steps))
+a+
+    geom_histogram(color="blue", fill="lightblue")+
+    labs(y="Number of Days", title = "Total Number of Steps")+
+    theme_bw()
+    
